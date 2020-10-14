95d5c23e2f65fadc488467bf85db5afe985f898e
BobMerks <bob.merks@hotmail.com>
2020-10-14 12:06:24 +0200

    Added app.js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -160,60 +160,6 @@
     </div>
   </section>
 </div>
+<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
 </body>
-
-<script>
-  let projects = false
-  let contact = false
-
-  let navElementProjects = document.getElementById("nav-projects")
-  let projectElement = document.getElementById("projects")
-
-  let navElementContact = document.getElementById("nav-contact")
-  let contactElement = document.getElementById("contact")
-
-  navElementProjects.addEventListener("click", () => {
-    projects ? setElements("p", "100vh", false, "My projects") : setElements("p", "0vh", true, "Close")
-  })
-
-  navElementContact.addEventListener("click", () => {
-    contact ? setElements("c", "100vh", false, "Contact") : setElements("c", "0vh", true, "Close")
-  })
-
-
-  let setElements = (e, margin, p, text) => {
-    if (e === "p"){
-      projectElement.style.marginTop = `${margin}`;
-      projectElement.style.zIndex = "10";
-      projects = p;
-      navElementProjects.innerText = `${text}`;
-      if (contact){
-        contactElement.style.marginTop = "100vh";
-        contactElement.style.zIndex = "1";
-        contact = false;
-        navElementContact.innerText = `Contact`;
-      }
-    }
-
-    else {
-      contactElement.style.marginTop = `${margin}`;
-      contactElement.style.zIndex = "10";
-      contact = p;
-      navElementContact.innerText = `${text}`;
-      if (projects){
-        projectElement.style.marginTop = "100vh";
-        projectElement.style.zIndex = "1";
-        projects = false;
-        navElementProjects.innerText = `My projects`;
-      }
-    }
-  }
-
-  window.onload = () => {
-    const landingText = document.getElementsByClassName("landing__line-text");
-    for (let i = 0; i < landingText.length; i++) {
-      landingText.item(i).classList.add("visible");
-    }
-  }
-</script>
 </html>
