90395fc3587bd7681ca1e33e57c09b0c9ffd88ce
BobMerks <bob.merks@hotmail.com>
2020-10-20 16:30:47 +0200

    Added github links and placeholder text for image

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,21 +45,20 @@
   </section>
 
   <section id="projects" class="projects">
-    <div id="projects-content" class="projects-content">
-      <!-- TODO: add links -->
+    <div class="projects-content">
       <div class="projects__container">
         <div class="projects__container-title">
           <span class="projects__container-title--text">This website</span>
           <span class="projects__container-title--date">October 2020</span>
         </div>
-        <div class="projects__container-image"></div>
+        <div class="projects__container-image"><p>Image coming soon.</p></div>
         <div class="projects__container-description">
           <p class="projects__container-text">A (re)make of my personal website, since I never really finished
-            the first version I had (which can be found on my GitHub as well). It's still a work in progress and it
+            the first version I had. It's still a work in progress and it
             isn't fully responsive yet and there are some features I want to add, like finishing my projects page.</p>
-{{--          <div class="projects__container-links">--}}
-{{--            <a class="projects__container-links-link">GitHub</a>--}}
-{{--          </div>--}}
+          <div class="projects__container-links">
+            <a href="https://github.com/BobMerks/PersonalWebsite" target="_blank" class="projects__container-links-link">GitHub</a>
+          </div>
         </div>
       </div>
 
@@ -68,16 +67,15 @@
           <span class="projects__container-title--text">JSON to radar graph</span>
           <span class="projects__container-title--date">June 2020</span>
         </div>
-        <div class="projects__container-image"></div>
+        <div class="projects__container-image"><p>Image coming soon.</p></div>
         <div class="projects__container-description">
           <p class="projects__container-text">During a school project we ran into an issue with converting existing JSON
             data, consisting of scores split up in various categories, to a radar graph showing these scores in a 0-100
             scale per category. I used JavaScript to take care of this problem and apply the data to a Chart.js radar
             graph.</p>
-{{--          <div class="projects__container-links">--}}
-{{--            <a class="projects__container-links-link">GitHub</a>--}}
-{{--            <a class="projects__container-links-link">Full website</a>--}}
-{{--          </div>--}}
+          <div class="projects__container-links">
+            <a href="https://github.com/BobMerks/JavaScriptJsonToRadar" target="_blank" class="projects__container-links-link">GitHub</a>
+          </div>
         </div>
       </div>
 
@@ -86,16 +84,15 @@
           <span class="projects__container-title--text">Sorting algorithms visualized</span>
           <span class="projects__container-title--date">March 2020</span>
         </div>
-        <div class="projects__container-image"></div>
+        <div class="projects__container-image"><p>Image coming soon.</p></div>
         <div class="projects__container-description">
           <p class="projects__container-text">
             I gained some interest in sorting algorithms and took the opportunity to improve my JavaScript skills with
             this simple website.
           </p>
-{{--          <div class="projects__container-links">--}}
-{{--            <a class="projects__container-links-link">GitHub</a>--}}
-{{--            <a class="projects__container-links-link">Full website</a>--}}
-{{--          </div>--}}
+          <div class="projects__container-links">
+            <a href="https://github.com/BobMerks/JavaScriptSorting" target="_blank" class="projects__container-links-link">GitHub</a>
+          </div>
         </div>
       </div>
 
@@ -104,7 +101,7 @@
           <span class="projects__container-title--text">Drag-and-drop website builder</span>
           <span class="projects__container-title--date">January 2020</span>
         </div>
-        <div class="projects__container-image"></div>
+        <div class="projects__container-image"><p>Image coming soon.</p></div>
         <div class="projects__container-description">
           <p class="projects__container-text">During my first internship I was asked to make a drag-and-drop website
             builder in JavaScript and Symfony 4, similar to Wordpress. It allowed the creation and editing of multiple
@@ -117,7 +114,7 @@
           <span class="projects__container-title--text">Movie list</span>
           <span class="projects__container-title--date">August 2019</span>
         </div>
-        <div class="projects__container-image"></div>
+        <div class="projects__container-image"><p>Image coming soon.</p></div>
         <div class="projects__container-description">
           <p class="projects__container-text">
             I wanted a way to keep track of what movies I watched and when, but an excel sheet wasn't good enough for
@@ -132,9 +129,9 @@
       <div class="projects__container">
         <div class="projects__container-title">
           <span class="projects__container-title--text"> Website PAL in OOPS</span>
-          <span class="projects__container-title--date">May 2019</span>
+          <span class="projects__container-title--date">January 2019</span>
         </div>
-        <div class="projects__container-image"></div>
+        <div class="projects__container-image"><p>Image coming soon.</p></div>
         <div class="projects__container-description">
           <p class="projects__container-text">
             The first website I ever did for someone else; a friend of mine that needed a simple website for his
